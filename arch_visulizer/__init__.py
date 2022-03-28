1d87479087fa70301f3d996727bd6a6b375bb5fd
jhs507 <jhs507@mail.usask.ca>
2022-03-28 12:14:47 -0600

    Graph data now calculated once before first reqest

--- arch_visulizer/__init__.py
+++ arch_visulizer/__init__.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template
+
+from arch_visulizer.diff_scenarios import buildgraph
 
 def create_app(test_config=None):
 
@@ -11,20 +13,62 @@ def create_app(test_config=None):
 
     if test_config is None:
         # load from the config file
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile('config.py')
     else:
         # Load from the passed in config
-        app.config.from_mapping(test_config)
+        app.config.from_pyfile(test_config)
 
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    #Test Page
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @app.before_first_request
+    def load_data():
+        app.graph_data = []
 
+        # Open each file and build a graph
+        for file_name in app.config['CALL_LOGS']:
+            g = buildgraph(open(file_name), 1)
+            f = os.path.basename(file_name)
+            app.graph_data.append((f, g))
+
+        nodes = []
+        edges = []
+        names = []
+        for data in app.graph_data:
+            nodes.append(list(data[1].nodes()))
+            edges.append(list(data[1].edges()))
+            names.append(data[0])
+
+
+
+
+    @app.route('/', methods=['GET'])
+    def index():
+
+        nodes = []
+        edges = []
+        names = []
+
+
+        for data in app.graph_data:
+            nodes.append(list(data[1].nodes()))
+            edges.append(list(data[1].edges()))
+            names.append(data[0])
+
+
+
+
+        perc=[]
+        for graph in app.graph_data:
+            perc.append(50)
+
+        return render_template(
+            'friendwheel.html',
+            sc1_nodes = nodes,
+            sc1_edges = edges,
+            sc2_nodes = nodes,
+            sc2_edges = edges,
+            sc1_filenames = names,
+            sc2_filenames = names,
+            perc = perc
+        )
 
     return app
