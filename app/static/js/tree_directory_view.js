ca6b62cef9dc7ee620787cd13b5f881c6b1994c8
Arup Ukil <arupukil@gmail.com>
2022-05-12 12:26:13 -0600

    Chnaged the visualisation of the comparisons of the two cluster trees to be more like a tree graph rather than directory

--- app/static/js/tree_directory_view.js
+++ app/static/js/tree_directory_view.js
@@ -25,70 +25,28 @@ function init() {
         diagrams[i - 1] =
             $(go.Diagram, "fullDiagram" + i,
                 {
-                    allowMove: false,
+                    allowMove: true,
                     allowCopy: false,
                     allowDelete: false,
                     allowHorizontalScroll: true,
                     layout:
                         $(go.TreeLayout,
                             {
-                                alignment: go.TreeLayout.AlignmentStart,
-                                angle: 0,
-                                compaction: go.TreeLayout.CompactionNone,
-                                layerSpacing: 16,
-                                layerSpacingParentOverlap: 1,
-                                nodeIndentPastParent: 1.0,
-                                nodeSpacing: 0,
-                                setsPortSpot: false,
-                                setsChildPortSpot: false
+                                angle: 90, // was 0 before
+                                layerSpacing: 35
                             })
                 });
         diagrams[i - 1].nodeTemplate =
             $(go.Node,
                 { // no Adornment: instead change panel background color by binding to Node.isSelected
                     selectionAdorned: false,
-                    isTreeExpanded: false,
-                    // a custom function to allow expanding/collapsing on double-click
-                    // this uses similar logic to a TreeExpanderButton
-                    doubleClick: function(e, node) {
-                        var cmd = diagrams[i - 1].commandHandler;
-                        if (node.isTreeExpanded) {
-                            if (!cmd.canCollapseTree(node)) return;
-                        } else {
-                            if (!cmd.canExpandTree(node)) return;
-                        }
-                        e.handled = true;
-                        if (node.isTreeExpanded) {
-                            cmd.collapseTree(node);
-                        } else {
-                            cmd.expandTree(node);
-                        }
-                    }
+                    isTreeExpanded: true
                 },
-                $("TreeExpanderButton",
-                    { // customize the button's appearance
-                        "_treeExpandedFigure": "ExpandedLine",
-                        "_treeCollapsedFigure": "CollapsedLine",
-                        "ButtonBorder.fill": "whitesmoke",
-                        "ButtonBorder.stroke": null,
-                        "_buttonFillOver": "rgba(0,128,255,0.25)",
-                        "_buttonStrokeOver": null
-                    }),
                 $(go.Panel, "Horizontal",
                     { position: new go.Point(18, 0) },
                     new go.Binding("background", "isSelected", function(s) { return (s ? "lightblue" : "white"); }).ofObject(),
-                    $(go.Picture,
-                        {
-                            width: 18, height: 18,
-                            margin: new go.Margin(0, 4, 0, 0),
-                            imageStretch: go.GraphObject.Uniform
-                        },
-                        // bind the picture source on two properties of the Node
-                        // to display open folder, closed folder, or document
-                        new go.Binding("source", "isTreeExpanded", imageConverter).ofObject(),
-                        new go.Binding("source", "isTreeLeaf", imageConverter).ofObject()),
                     $(go.TextBlock,
-                        { font: '9pt Verdana, sans-serif'},
+                        { font: '9pt Verdana, sans-serif', width: 100},
                         new go.Binding("text", "node_text", function(s) { return " " + s; }),
                         new go.Binding('stroke', 'color')
                     )
@@ -96,7 +54,25 @@ function init() {
             );  // end Node
 
         // without lines
-        diagrams[i - 1].linkTemplate = $(go.Link);
+        diagrams[i - 1].linkTemplate =
+            $(go.Link,
+                {
+                    routing: go.Link.Orthogonal,
+                    corner: 5
+                },
+                $(go.Shape,
+                    {
+                        strokeWidth: 3,
+                        stroke: "#555"
+                    }
+                ),
+                $(go.Shape,
+                    {
+                        toArrow: "Standard",
+                        stroke: null
+                    }
+                )
+            );
 
 
         diagrams[i - 1].addDiagramListener("ObjectContextClicked",
