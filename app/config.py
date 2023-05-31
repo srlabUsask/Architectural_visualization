63d83186bd33741d8a3bbcb3da95eca6e205b8e9
jhs507 <jhs507@mail.usask.ca>
2023-05-31 13:02:36 -0600

    Cleaned up the repository for renewed development

--- app/config.py
+++ app/config.py
@@ -1,6 +1,4 @@
-# Currently set to be used in IDEs like Pycharm rather than the shell
-ROOT = 'C:/Users/aru470/Repos/CRHM/Architectural_visualization/app'  # directory location of app folder for the server run
-# /mnt/c/Users/arupu/Documents/CHRM/AbstractCodeSummaryTree/app
-SUBJECT_SYSTEM_NAME = '12.4.4'  # Task_1_Jun-08-21 BadlakeRun Task_2_Feb-04-22 Task_1_Feb-04-22 Task_1_Nov-19-20 jupyter_client_10_16_2020 test2_06-01-2022
+ROOT = '' # directory location of app folder for the server run
+SUBJECT_SYSTEM_NAME = 'Task_1_Jun-08-21' #Task_1_Jun-08-21 BadlakeRun Task_2_Feb-04-22 Task_1_Feb-04-22 Task_1_Nov-19-20 jupyter_client_10_16_2020 test2_06-01-2022
 EXECUTION_PATTERNS = False
 DOC2VEC = False
