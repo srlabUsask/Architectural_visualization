3fae59bd7fa129ac31a6da179dadd93f94bbe292
Narek Tarmazyan <narek660592@gmail.com>
2023-07-18 11:04:51 -0600

    Removed callLog parser file (it will not be used)
    removed callLogParser.py

--- app/callLogParser.py
+++
@@ -1,4 +0,0 @@
-ROOT = '/mnt/c/Users/uwo916/source/repos/clone/CRHM/Architectural_visualization/app' # directory location of app folder for the server run
-SUBJECT_SYSTEM_NAME = 'CRHMCODE_Trace_6_29' #Task_1_Jun-08-21 BadlakeRun Task_2_Feb-04-22 Task_1_Feb-04-22 Task_1_Nov-19-20 jupyter_client_10_16_2020 test2_06-01-2022
-EXECUTION_PATTERNS = True
-DOC2VEC = False
